c6ca4f126f3617ef6bfeaed65c612e5f76f4200a
Claire <claire.solf@dci-student.org>
2024-07-08 15:22:56 +0200

    soweit so gut!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HeilmittelFinder
+
+Eine kleine Spielerei, die man sicher noch tütig ausbauen kann. Man gibt ein "Problem" ein und erhält einen Heilstein und eine Heilpflanze ausgegeben.
--- heilung.js
+++ heilung.js
@@ -1,1 +1,31 @@
-heilung.js
+const heilmittel = {
+    stress: {stein: 'Amethyst', pflanze: 'Lavendel'},
+    angst: {stein: 'Lepidolith', pflanze: 'Kamille'},
+    kopfschmerzen: {stein: 'Sodalith', pflanze: 'Pfefferminze'},
+    müdigkeit: {stein: 'Citrin', pflanze: 'Ginseng'},
+    verdauungsbeschwerden: {stein: 'Malachit', pflanze: 'Ingwer'},
+    liebeskummer: {stein: 'Rosenquarz', pflanze: 'Herzgespann'},
+    stiche: {stein: 'Achat', pflanze: 'Aloe Vera'},
+    katergefühl: {stein: 'Amethyst', pflanze: 'Mariendistel'}
+};
+
+function findeMedizin (problem) {
+    return heilmittel[problem.toLowerCase()];
+}
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Welches Problem hast du? ', (problem) => {
+    const medizin = findeMedizin(problem);
+    if (medizin){
+        console.log(`Für ${problem} empfehle ich den Heilstein ${medizin.stein} und die Heilpflanze ${medizin.pflanze}.`);
+    } else {
+        console.log('Für dieses Problem habe ich leider keinen Vorschlag.');
+    }
+rl.close();
+});
